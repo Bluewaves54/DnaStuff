82f218460f6f43e3f1c21789ca25a5b261bd03e8
Bluewaves54 <ayush_p@icloud.com>
2021-08-10 19:24:19 -0400

    renaming & deleting files

--- coding4medicineday1.py
+++
@@ -1,3 +0,0 @@
-x = range(55, 100, 2)
-print(x)
-print(range(99, 54, -2))
--- find_real_protein.py
+++ find_real_protein.py
@@ -29,32 +29,36 @@ amino_acids = {'F': ['UUU', 'UUC'],
 stop_codons = ['UAA', 'UAG', 'UGA']
 acid_letters = amino_acids.keys()
 
-f = open('/Users/ayushpal/Library/Mobile Documents/com~apple~TextEdit/Documents/SARS_genome.txt').readlines()
+f = open('SARS_genome.txt').readlines()
 f.remove(f[0])
 sars_genome = ''
 for i in range(0, len(f) - 1):
-    sars_genome += f[i]
+    sars_genome += f[i].rstrip()
+
+
 start = int(input('what is the starting point? (1, 2, or 3): '))
-dna_seq = list(sars_genome.upper())
-for i in range(0, len(dna_seq)-1):
-    if dna_seq[i] == 'T':
-        dna_seq[i] = 'U'
+sars_genome = list(sars_genome.upper())
+for i in range(0, len(sars_genome) - 1):
+    if sars_genome[i] == 'T':
+        sars_genome[i] = 'U'
 
 
 protein_seq = []
-index1 = start - 1
+start_index = start - 1
 end_index = start + 2
-keep_going = True
-while index1 < len(dna_seq) and keep_going:
-    if list_to_string(dna_seq[index1:end_index]) in stop_codons:
+# keep_going = True
+while start_index < len(sars_genome):
+    if list_to_string(sars_genome[start_index:end_index]) in stop_codons:
         protein_seq.append('*')
     for i in acid_letters:
-        if list_to_string(dna_seq[index1:end_index]) in amino_acids[i]:
+        if list_to_string(sars_genome[start_index:end_index]) in amino_acids[i]:
             protein_seq.append(i)
 
-    index1 += 3
+    start_index += 3
     end_index += 3
-stop_codon_indices, lengths = [], []
+
+
+end_indices, lengths = [], []
 count = 0
 for i in range(0, len(protein_seq) - 1):
     if protein_seq[i] != '*':
@@ -62,18 +66,26 @@ for i in range(0, len(protein_seq) - 1):
     if protein_seq[i] == '*' and count < 100:
         count = 0
     if protein_seq[i] == '*' and count > 100:
-        stop_codon_indices.append(i)
+        end_indices.append(i)
         lengths.append(count)
         count = 0
 
 
 real_proteins = ''
 list_to_string(protein_seq)
-for i in range(0, len(stop_codon_indices)):
-    index1 = stop_codon_indices[i] - lengths[i] - 1
-    index2 = stop_codon_indices[i] + 1
-    real_proteins += list_to_string(protein_seq[index1:index2])
+for i in range(0, len(end_indices)):
+    pro_start_index = end_indices[i] - lengths[i]
+    seq_length = lengths[i]
+    pro_end_index = end_indices[i]
+    dna_start_index = (pro_start_index * 3 + start - 1)
+    dna_stop_index = (pro_end_index * 3 + start - 1)
+    real_proteins += list_to_string(protein_seq[pro_start_index:pro_end_index])
     real_proteins += '(at '
-    real_proteins += str(index1)
-    real_proteins += '), '
-print('The real protein sequences are', real_proteins)
+    real_proteins += str(dna_start_index)
+    real_proteins += ', '
+    real_proteins += str(dna_stop_index)
+    real_proteins += '), \n'
+    print(dna_start_index, dna_stop_index, seq_length)
+
+
+print('The real protein sequences are\n', real_proteins.lstrip())
