d0418b7887c99af5b2606041f8ada2a18ef97548
Bluewaves54 <ayush_p@icloud.com>
2021-08-10 22:59:18 -0400

    renaming & deleting files

--- find_real_protein.py
+++ find_real_protein.py
@@ -29,63 +29,63 @@ amino_acids = {'F': ['UUU', 'UUC'],
 stop_codons = ['UAA', 'UAG', 'UGA']
 acid_letters = amino_acids.keys()
 
-f = open('SARS_genome.txt').readlines()
+file = str(input('file directory: '))
+f = open(file).readlines()
 f.remove(f[0])
-sars_genome = ''
-for i in range(0, len(f) - 1):
-    sars_genome += f[i].rstrip()
+genome = ''
+for line in range(0, len(f) - 1):
+    genome += f[line].rstrip()
 
 
-start = int(input('what is the starting point? (1, 2, or 3): '))
-sars_genome = list(sars_genome.upper())
-for i in range(0, len(sars_genome) - 1):
-    if sars_genome[i] == 'T':
-        sars_genome[i] = 'U'
+genome = list(genome.upper())
+for t in range(0, len(genome) - 1):
+    if genome[t] == 'T':
+        genome[t] = 'U'
 
 
-protein_seq = []
-start_index = start - 1
-end_index = start + 2
-# keep_going = True
-while start_index < len(sars_genome):
-    if list_to_string(sars_genome[start_index:end_index]) in stop_codons:
-        protein_seq.append('*')
-    for i in acid_letters:
-        if list_to_string(sars_genome[start_index:end_index]) in amino_acids[i]:
-            protein_seq.append(i)
+def find_real_protein(x):
+    protein_seq = []
+    start_index = x
+    end_index = x + 3
 
-    start_index += 3
-    end_index += 3
+    while start_index < len(genome):
+        if list_to_string(genome[start_index:end_index]) in stop_codons:
+            protein_seq.append('*')
+        for i in acid_letters:
+            if list_to_string(genome[start_index:end_index]) in amino_acids[i]:
+                protein_seq.append(i)
 
+        start_index += 3
+        end_index += 3
 
-end_indices, lengths = [], []
-count = 0
-for i in range(0, len(protein_seq) - 1):
-    if protein_seq[i] != '*':
-        count += 1
-    if protein_seq[i] == '*' and count < 100:
-        count = 0
-    if protein_seq[i] == '*' and count > 100:
-        end_indices.append(i)
-        lengths.append(count)
-        count = 0
+    end_indices, lengths = [], []
+    count = 0
+    for i in range(0, len(protein_seq) - 1):
+        if protein_seq[i] != '*':
+            count += 1
+        if protein_seq[i] == '*' and count < 100:
+            count = 0
+        if protein_seq[i] == '*' and count > 100:
+            end_indices.append(i)
+            lengths.append(count)
+            count = 0
 
+    real_proteins = []
+    list_to_string(protein_seq)
+    for i in range(0, len(end_indices)):
+        pro_start_index = end_indices[i] - lengths[i]
+        seq_length = lengths[i]
+        pro_end_index = end_indices[i]
+        dna_start_index = (pro_start_index * 3 + start - 1)
+        dna_stop_index = (pro_end_index * 3 + start - 1)
+        p = (list_to_string(protein_seq[pro_start_index:pro_end_index]), '(', str(seq_length),
+             ' characters long at ', str(dna_start_index), ', ', str(dna_stop_index), '), \n')
+        protein = ''.join(p)
 
-real_proteins = ''
-list_to_string(protein_seq)
-for i in range(0, len(end_indices)):
-    pro_start_index = end_indices[i] - lengths[i]
-    seq_length = lengths[i]
-    pro_end_index = end_indices[i]
-    dna_start_index = (pro_start_index * 3 + start - 1)
-    dna_stop_index = (pro_end_index * 3 + start - 1)
-    real_proteins += list_to_string(protein_seq[pro_start_index:pro_end_index])
-    real_proteins += '(at '
-    real_proteins += str(dna_start_index)
-    real_proteins += ', '
-    real_proteins += str(dna_stop_index)
-    real_proteins += '), \n'
-    print(dna_start_index, dna_stop_index, seq_length)
+    return protein
 
 
-print('The real protein sequences are\n', real_proteins.lstrip())
+all_real_proteins = ''
+for start in range(0, 3):
+    all_real_proteins += find_real_protein(start)
+print('The real protein sequences are\n', all_real_proteins)
